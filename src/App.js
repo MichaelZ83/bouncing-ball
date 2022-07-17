202c42ada0aa1a48f75acbca54f799a8994f543f
MichaelZ83 <51152849+MichaelZ83@users.noreply.github.com>
2022-07-17 10:36:55 +0000

    initial push

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './index.css';
+
+export default function App() {
+  return (
+    <div className="scene">
+      <div className='floor'></div>
+      <div className='cube'></div>
+      <div className='ball'></div>
+    </div>
+  );
+}
